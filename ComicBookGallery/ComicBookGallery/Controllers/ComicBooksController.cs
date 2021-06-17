15f9b291633da5ddb64b0407f22b7966d58668c2
Brienna Klassen <brienna.klassen@gmail.com>
2021-06-17 15:17:14 -0700

    add detail view

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -6,14 +6,8 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail()
-        { 
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-            return Redirect("/");
-            }
-
-            return Content("Hello, from the comic books controller!");
-            
+        {
+            return View();
         }
     }
 }
