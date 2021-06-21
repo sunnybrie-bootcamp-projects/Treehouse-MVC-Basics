8da4315bf7f1db4b97081d396929684f06945cb8
Brienna Klassen <brienna.klassen@gmail.com>
2021-06-21 15:26:15 -0700

    add comic book models

---
+++ ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
